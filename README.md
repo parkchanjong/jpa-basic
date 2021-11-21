0a2c7f959664d9712819b72e6fe23a38eaebc8e6
parkchanjong <7000cj@gmail.com>
2021-11-21 15:41:54 +0900

    JPA CRUD

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # jpa-basic
+
+### JPA
+- EntityManagerFactory 는 애플리케이션에 1개만 생성
+- EntityManager를 통해서 작업
+- 모든 작업은 트랜젝션 안에서 작업
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -13,14 +14,33 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
-        em.persist(member);
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloA");
+//
+//            em.persist(member);
 
-        tx.commit();
 
-        em.close();
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("helloJPA2");
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
