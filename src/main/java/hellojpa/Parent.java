7f44082cf3dc87e581dcee40a283d49842dca27d
parkchanjong <7000cj@gmail.com>
2021-11-27 16:27:06 +0900

    JPA cascade

--- src/main/java/hellojpa/Parent.java
+++ src/main/java/hellojpa/Parent.java
@@ -1,2 +1,47 @@
-package hellojpa;public class Parent {
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.PERSIST)
+    private List<Chlid> chlidList = new ArrayList<>();
+
+    public void addChlid(Chlid chlid) {
+        chlidList.add(chlid);
+        chlid.setParent(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Chlid> getChlidList() {
+        return chlidList;
+    }
+
+    public void setChlidList(List<Chlid> chlidList) {
+        this.chlidList = chlidList;
+    }
 }
